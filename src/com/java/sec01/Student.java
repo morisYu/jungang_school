42858c012c9584e8e726fb86c7da34497b0ee81b
morisYu <morisyu50@gmail.com>
2022-01-17 14:53:57 +0900

    second

--- src/com/java/sec01/Student.java
+++ src/com/java/sec01/Student.java
@@ -1,5 +1,10 @@
 package com.java.sec01;
 
 public class Student {
+	String name;
+	int count;
 
+	public void check() {
+		System.out.println(name + count);
+	}
 }
--- src/com/java/sec01/StudentExample.java
+++ src/com/java/sec01/StudentExample.java
@@ -9,7 +9,10 @@ public class StudentExample {
 		Student s2 = new Student();
 		System.out.println("s2 변수가 Student 객체를 참조합니다.");
 		System.out.println(s2);
-		
+
+		s1.name = "김개똥";
+		s1.count = 10;
+		s1.check();
 
 	}
 }
