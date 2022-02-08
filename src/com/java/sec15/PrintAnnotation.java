eb9adf821a6fd12445e0623ef0746a925a74635e
morisyu <morisyu50@gmail.com>
2022-02-08 16:33:14 +0900

    6장 접근제한, Annotation

---
+++ src/com/java/sec15/PrintAnnotation.java
@@ -0,0 +1,15 @@
+package com.java.sec15;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface PrintAnnotation {
+
+	String value() default "-";
+
+	int number() default 15;
+}
---
+++ src/sec13/exam01_class_access/package2/C.java
@@ -0,0 +1,8 @@
+package sec13.exam01_class_access.package2;
+
+import sec13.exam01_class_access.package1.*;
+
+public class C {
+	// A a; 패키지 위치가 다르고 default 접근제한자이기 때문에 불러올 수 없음
+	B b;
+}
