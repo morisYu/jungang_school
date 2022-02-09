39f9c7ae5a227257b207d916db8372359d62229f
morisyu <morisyu50@gmail.com>
2022-02-09 15:27:36 +0900

    7장 7.1~7.4절

---
+++ src/com/java/sec02/DmbCellPhoneEaxmple.java
@@ -0,0 +1,29 @@
+package com.java.sec02;
+
+public class DmbCellPhoneEaxmple {
+
+	public static void main(String[] args) {
+		// DmbCellPhone 객체 생성
+		DmbCellPhone dmbCellPhone = new DmbCellPhone("자바폰", "검정", 10);
+
+		// CellPhone 으로부터 상속받은 필드
+		System.out.println("모델: " + dmbCellPhone.model);
+		System.out.println("모델: " + dmbCellPhone.color);
+
+		// DmbCellPhone의 필드
+		System.out.println("채널: " + dmbCellPhone.channel);
+
+		// CellPhone 으로부터 상속받은 메소드 호출
+		dmbCellPhone.powerOn();
+		dmbCellPhone.bell();
+		dmbCellPhone.sendVoice("여보세요");
+		dmbCellPhone.receiveVoice("안녕하세요. 홍길동");
+		dmbCellPhone.sendVoice("아~ 예 반갑");
+		dmbCellPhone.hangUp();
+
+		// DmbCellPhone 의 메소드 호출
+		dmbCellPhone.turnOnDmb();
+		dmbCellPhone.changeChannelDmb(12);
+		dmbCellPhone.turnOffDmb();
+	}
+}
---
+++ src/com/java/sec03/People.java
@@ -0,0 +1,13 @@
+package com.java.sec03;
+
+// 부모 클래스(super)
+
+public class People {
+	public String name;
+	public String ssn;
+
+	public People(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+}
